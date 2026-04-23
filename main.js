946ec760c92d6fae03f22c92439e42c1f7a9bf15
liovoz <liovoz1983@protonmail.com>
2026-04-23 13:50:08 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const os = require('os');
 const path = require('path');
 const { autoUpdater } = require('electron-updater'); 
 
-// 【底层安全防御】：提前创建 anonymous_token，防止 API 库首次运行崩溃
+// 【底层安全防御】：提前创建 anonymous_token
 try {
     const tokenPath = path.join(os.tmpdir(), 'anonymous_token');
     if (!fs.existsSync(tokenPath)) {
@@ -14,14 +14,13 @@ try {
     console.error('预创建 token 文件失败:', e);
 }
 
-// 引入内置网易云 API 引擎
 const { serveNcmApi } = require('NeteaseCloudMusicApi/server');
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 980,
+        width: 1020, 
         height: 720,
         autoHideMenuBar: true,
         webPreferences: {
@@ -34,38 +33,31 @@ function createWindow() {
 
 app.whenReady().then(async () => {
     try {
-        // 1. 尝试启动内置网易云 API 引擎
         await serveNcmApi({
             port: 3000,
             checkVersion: false
         });
         console.log('网易云 API 启动成功！');
         
-        // 2. 只有 API 确认启动成功了，才打开主界面
         createWindow();
 
-        // 3. 软件启动后，如果是打包后的版本，延迟 3 秒悄悄检查更新
         if (app.isPackaged) {
             setTimeout(() => {
                 autoUpdater.checkForUpdatesAndNotify();
             }, 3000);
         }
-
     } catch (error) {
-        // 如果后台崩溃，立刻弹窗报错
         dialog.showErrorBox('API 启动惨遭失败', '具体原因: ' + error.message);
     }
 });
 
-// ====== 自动更新事件监听 ======
 if (app.isPackaged) {
-    // 当后台静默下载完新版本后，弹出提示框让用户重启安装
     autoUpdater.on('update-downloaded', (info) => {
         dialog.showMessageBox({
             type: 'info',
             title: '🎉 发现新版本！',
             message: `MusicZ 最新版 ${info.version} 已经为你准备好了！`,
-            detail: '是否立即重启软件体验新功能？（选择稍后则在下次打开软件时自动更新）',
+            detail: '是否立即重启软件体验新功能？',
             buttons: ['立即重启并更新', '稍后再说'],
             cancelId: 1
         }).then((result) => {
